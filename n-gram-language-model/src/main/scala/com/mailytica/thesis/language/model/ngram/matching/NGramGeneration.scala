2fbf5fb0e82efeeaef8c3469fbe7acbf3ee50c5f
Anna Bernauer <an851ber@htwg-kontanz.de>
2021-03-12 11:59:38 +0100

    NGramGeneration added

---
+++ n-gram-language-model/src/main/scala/com/mailytica/thesis/language/model/ngram/matching/NGramGeneration.scala
@@ -0,0 +1,17 @@
+package com.mailytica.thesis.language.model.ngram.matching
+import com.johnsnowlabs.nlp.annotators.NGramGenerator
+import org.apache.spark.ml.PipelineStage
+
+object NGramGeneration extends AbstractMatching {
+
+  override def getSpecificStages(): Array[_ <: PipelineStage] = {
+    val nGramGenerator = new NGramGenerator()
+      .setInputCols("token")
+      .setOutputCol("ngrams")
+      .setN(2)
+      .setEnableCumulative(true)
+      .setDelimiter("_")
+
+    Array(nGramGenerator)
+  }
+}
---
+++ n-gram-language-model/src/test/scala/com/mailytica/thesis/language/model/ngram/matching/NGramGenerationSpec.scala
@@ -0,0 +1,40 @@
+package com.mailytica.thesis.language.model.ngram.matching
+
+import com.johnsnowlabs.nlp.{Annotation, LightPipeline}
+import com.johnsnowlabs.nlp.util.io.ResourceHelper
+import com.mailytica.thesis.language.model.ngram.matching.NGramGeneration.{getGeneralStages, getSpecificStages}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, WordSpec}
+
+@RunWith(classOf[JUnitRunner])
+class NGramGenerationSpec extends WordSpec with Matchers {
+
+  "A text" when {
+    ResourceHelper.spark
+
+    import ResourceHelper.spark.implicits._
+
+    val nlpPipeline = new Pipeline()
+
+    nlpPipeline.setStages(getGeneralStages() ++ getSpecificStages())
+
+    val pipelineModel: PipelineModel = nlpPipeline.fit(Seq.empty[String].toDF("text"))
+
+    "has matches" should {
+
+      val textWithMatches = """Quantum test million"""
+
+
+      val annotated: Map[String, Seq[Annotation]] = new LightPipeline(pipelineModel).fullAnnotate(textWithMatches)
+      annotated.foreach(println)
+
+      "have the correct entries" in {
+        annotated("ngrams").head.result should be("Quantum")
+        annotated("ngrams").last.result should be("test_million")
+        annotated("ngrams").size should be(5)
+      }
+    }
+  }
+}
